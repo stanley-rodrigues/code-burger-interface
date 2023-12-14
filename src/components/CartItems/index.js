1158693da5d771682b50c15a94a1b893717bab8f
Stanley-Rodrigues <stanleyrodriguessilva@gmail.com>
2023-12-14 14:31:40 -0300

    Add styles cartResume

--- src/components/CartItems/index.js
+++ src/components/CartItems/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 import { useCart } from '../../hooks/CartContext'
 import formatCurrency from '../../utils/formatCurrency'
-import { Container, Header, Body } from './styles'
+import { Container, Header, Body, EmptyCart } from './styles'
 
 export function CartItems() {
-  const { cartProducts } = useCart()
+  const { cartProducts, increaseProducts, decreaseProducts } = useCart()
   console.log(cartProducts)
   return (
     <Container>
@@ -13,19 +13,27 @@ export function CartItems() {
         <p></p>
         <p>Itens</p>
         <p>Preço</p>
-        <p>Quantidade</p>
+        <p style={({ paddingRight: 30 }, { paddingLeft: -15 })}>Quantidade</p>
         <p>Total</p>
       </Header>
-      {cartProducts &&
+      {cartProducts && cartProducts.length > 0 ? (
         cartProducts.map(product => (
           <Body key={product.id}>
             <img src={product.url} />
             <p>{product.name}</p>
             <p>{formatCurrency(product.price)}</p>
-            <p>{product.quantity}</p>
+            <div className="quantity-container">
+              <button onClick={() => decreaseProducts(product.id)}>-</button>
+              <p>{product.quantity}</p>
+              <button onClick={() => increaseProducts(product.id)}>+</button>
+            </div>
+
             <p>{formatCurrency(product.quantity * product.price)}</p>
           </Body>
-        ))}
+        ))
+      ) : (
+        <EmptyCart>Seu carrinho está vazio!</EmptyCart>
+      )}
     </Container>
   )
 }
--- src/hooks/CartContext.js
+++ src/hooks/CartContext.js
@@ -5,6 +5,10 @@ const CartContext = createContext({})
 export const CartProvider = ({ children }) => {
   const [cartProducts, setCartProducts] = useState([])
 
+  const updateLocalStorage = async products => {
+    await localStorage.setItem('codeburger:cartInfo', JSON.stringify(products))
+  }
+
   const putProductInCart = async product => {
     const cartIndex = cartProducts.findIndex(prd => prd.id === product.id)
     let newCartProducts = []
@@ -20,10 +24,43 @@ export const CartProvider = ({ children }) => {
       setCartProducts(newCartProducts)
     }
 
-    await localStorage.setItem(
-      'codeburger:cartInfo',
-      JSON.stringify(newCartProducts)
-    )
+    await updateLocalStorage(newCartProducts)
+  }
+
+  const deleteProducts = async productId => {
+    const newCart = cartProducts.filter(product => product.id !== productId)
+    setCartProducts(newCart)
+
+    await updateLocalStorage(newCart)
+  }
+
+  const increaseProducts = async productId => {
+    const newCart = cartProducts.map(product => {
+      return product.id === productId
+        ? { ...product, quantity: product.quantity + 1 }
+        : product
+    })
+
+    setCartProducts(newCart)
+
+    await updateLocalStorage(newCart)
+  }
+
+  const decreaseProducts = async productId => {
+    const cartIndex = cartProducts.findIndex(pd => pd.id === productId)
+
+    if (cartProducts[cartIndex].quantity > 1) {
+      const newCart = cartProducts.map(product => {
+        return product.id === productId
+          ? { ...product, quantity: product.quantity - 1 }
+          : product
+      })
+      setCartProducts(newCart)
+
+      await updateLocalStorage(newCart)
+    } else {
+      deleteProducts(productId)
+    }
   }
 
   useEffect(() => {
@@ -39,7 +76,14 @@ export const CartProvider = ({ children }) => {
   }, [])
 
   return (
-    <CartContext.Provider value={{ putProductInCart, cartProducts }}>
+    <CartContext.Provider
+      value={{
+        putProductInCart,
+        cartProducts,
+        increaseProducts,
+        decreaseProducts
+      }}
+    >
       {children}
     </CartContext.Provider>
   )
